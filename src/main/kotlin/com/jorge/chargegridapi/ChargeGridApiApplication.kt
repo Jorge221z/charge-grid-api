c7e6361dd99a95229723e13585dad4c8a8bcdbad
Jorge221z <jorgemunozcast12@gmail.com>
2026-03-18 20:55:13 +0100

    Initial commit: Spring Boot setup for ChargeGrid

---
+++ src/main/kotlin/com/jorge/chargegridapi/ChargeGridApiApplication.kt
@@ -0,0 +1,11 @@
+package com.jorge.chargegridapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ChargeGridApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<ChargeGridApiApplication>(*args)
+}
